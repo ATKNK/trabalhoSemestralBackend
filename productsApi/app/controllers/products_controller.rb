dd5f4c87613abf41bc2317ccff9a68eb7020d8cc
Arthur Nakayama <arthurnakayama@gmail.com>
2026-05-05 03:01:13 -0300

    feat: added validations for product name and price

--- productsApi/app/controllers/products_controller.rb
+++ productsApi/app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :name, :description, :price ])
+      params.require(:product).permit(:name, :price)
     end
 end
